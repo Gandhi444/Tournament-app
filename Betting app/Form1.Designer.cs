9381a0b9b47904369c4db6acbdf28eaf1124bf61
Gandhi444 <67520325+Gandhi444@users.noreply.github.com>
2022-07-06 16:11:29 +0200

    Add project files.

---
+++ Betting app/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Betting_app
+{
+    partial class FIR
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MatchList = new System.Windows.Forms.ComboBox();
+            this.Playing1 = new System.Windows.Forms.Label();
+            this.Draw = new System.Windows.Forms.Label();
+            this.Playing2 = new System.Windows.Forms.Label();
+            this.Lock = new System.Windows.Forms.Button();
+            this.Wynik = new System.Windows.Forms.Label();
+            this.score1 = new System.Windows.Forms.NumericUpDown();
+            this.score2 = new System.Windows.Forms.NumericUpDown();
+            this.LockScore = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.score1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.score2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MatchList
+            // 
+            this.MatchList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.MatchList.FormattingEnabled = true;
+            this.MatchList.Location = new System.Drawing.Point(96, 12);
+            this.MatchList.MaxDropDownItems = 21;
+            this.MatchList.Name = "MatchList";
+            this.MatchList.Size = new System.Drawing.Size(121, 21);
+            this.MatchList.TabIndex = 3;
+            this.MatchList.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Playing1
+            // 
+            this.Playing1.AutoSize = true;
+            this.Playing1.Location = new System.Drawing.Point(96, 52);
+            this.Playing1.Name = "Playing1";
+            this.Playing1.Size = new System.Drawing.Size(0, 13);
+            this.Playing1.TabIndex = 10;
+            // 
+            // Draw
+            // 
+            this.Draw.AutoSize = true;
+            this.Draw.Location = new System.Drawing.Point(139, 52);
+            this.Draw.Name = "Draw";
+            this.Draw.Size = new System.Drawing.Size(32, 13);
+            this.Draw.TabIndex = 11;
+            this.Draw.Text = "Draw";
+            // 
+            // Playing2
+            // 
+            this.Playing2.AutoSize = true;
+            this.Playing2.Location = new System.Drawing.Point(182, 52);
+            this.Playing2.Name = "Playing2";
+            this.Playing2.Size = new System.Drawing.Size(0, 13);
+            this.Playing2.TabIndex = 26;
+            // 
+            // Lock
+            // 
+            this.Lock.Location = new System.Drawing.Point(99, 280);
+            this.Lock.Name = "Lock";
+            this.Lock.Size = new System.Drawing.Size(118, 23);
+            this.Lock.TabIndex = 34;
+            this.Lock.Text = "LOCK BET";
+            this.Lock.UseVisualStyleBackColor = true;
+            this.Lock.Click += new System.EventHandler(this.Lock_Click);
+            // 
+            // Wynik
+            // 
+            this.Wynik.AutoSize = true;
+            this.Wynik.Location = new System.Drawing.Point(139, 337);
+            this.Wynik.Name = "Wynik";
+            this.Wynik.Size = new System.Drawing.Size(35, 13);
+            this.Wynik.TabIndex = 35;
+            this.Wynik.Text = "Score";
+            // 
+            // score1
+            // 
+            this.score1.Location = new System.Drawing.Point(98, 364);
+            this.score1.Name = "score1";
+            this.score1.Size = new System.Drawing.Size(36, 20);
+            this.score1.TabIndex = 36;
+            // 
+            // score2
+            // 
+            this.score2.Location = new System.Drawing.Point(180, 364);
+            this.score2.Name = "score2";
+            this.score2.Size = new System.Drawing.Size(36, 20);
+            this.score2.TabIndex = 37;
+            // 
+            // LockScore
+            // 
+            this.LockScore.Location = new System.Drawing.Point(99, 403);
+            this.LockScore.Name = "LockScore";
+            this.LockScore.Size = new System.Drawing.Size(118, 23);
+            this.LockScore.TabIndex = 38;
+            this.LockScore.Text = "LOCK SCORE";
+            this.LockScore.UseVisualStyleBackColor = true;
+            this.LockScore.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // FIR
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(787, 451);
+            this.Controls.Add(this.LockScore);
+            this.Controls.Add(this.score2);
+            this.Controls.Add(this.score1);
+            this.Controls.Add(this.Wynik);
+            this.Controls.Add(this.Lock);
+            this.Controls.Add(this.Playing2);
+            this.Controls.Add(this.Draw);
+            this.Controls.Add(this.Playing1);
+            this.Controls.Add(this.MatchList);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "FIR";
+            this.ShowIcon = false;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FIR_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.score1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.score2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ComboBox MatchList;
+        private System.Windows.Forms.Label Playing1;
+        private System.Windows.Forms.Label Draw;
+        private System.Windows.Forms.Label Playing2;
+        private System.Windows.Forms.Button Lock;
+        private System.Windows.Forms.Label Wynik;
+        private System.Windows.Forms.NumericUpDown score1;
+        private System.Windows.Forms.NumericUpDown score2;
+        private System.Windows.Forms.Button LockScore;
+    }
+}
+
---
+++ Betting app/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Betting_app
+{
+    public class Player
+    {
+        public Player(string name)
+        {
+            Name = name;
+        }
+        public string Name;
+        public int Bilans;
+        public int Cash;
+        public string[] boxes;
+        public int wins;
+        public int draws;
+        public int loses;
+    }
+
+    public partial class FIR : Form
+    {
+        string[] Matches;
+        bool[,,] bet_state;
+        bool[] bet_locks;
+        int Player_nr;
+        int[] scores;
+        Player[] players;
+        public void Create_new_box(string name, int x, int y)
+        {
+            CheckBox b = new CheckBox();
+            b.Name = name;
+            b.AutoSize = true;
+            b.Location = new System.Drawing.Point(106 + 43 * x, 74 + 26 * y);
+            b.Size = new System.Drawing.Size(15, 14);
+            b.UseVisualStyleBackColor = true;
+            b.CheckedChanged += new System.EventHandler(this.Box_click_handler);
+            Controls.Add(b);
+        }
+        public void load_matches_and_players()
+        {
+            Matches = System.IO.File.ReadAllLines("Matches.txt");
+            string[] Players_names = System.IO.File.ReadAllLines("Players.txt");
+            Player_nr = Players_names.Length;
+            for (int i = 0; i < Matches.Length; i++)
+            {
+                string[] subs = Matches[i].Split(' ');
+                Matches[i] = String.Concat((i + 1).ToString(), ". ", subs[0], " vs ", subs[1]);
+            }
+            bet_state = new bool[Matches.Length, Player_nr, 3];
+            bet_locks = new bool[Matches.Length];
+            scores = new int[Matches.Length];
+            players = new Player[Player_nr];
+            for (int i = 0; i < Player_nr; i++)
+            {
+                players[i] = new Player(Players_names[i]);
+                Label player_label = new Label();
+                player_label.AutoSize = true;
+                player_label.Location = new System.Drawing.Point(12, 75 + 25 * (i));
+                player_label.Name = Players_names[i];
+                player_label.Text = Players_names[i];
+                Controls.Add(player_label);
+                string[] buf_names = new string[3];
+                for (int j = 0; j < 3; j++)
+                {
+                    string name = string.Concat("P", (i + 1).ToString(), "B", j.ToString());
+                    buf_names[j] = name;
+                    Create_new_box(name, j, i);
+                }
+                players[i].boxes = buf_names;
+            }
+        }
+        public void update_boxes(object sender)
+        {
+            CheckBox chk = (CheckBox)sender;
+            if (chk.Checked)
+            {
+                for (int i = 0; i < Player_nr; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        if (players[i].boxes[j] == chk.Name)
+                        {
+                            CheckBox buf1 = (CheckBox)Controls.Find(players[i].boxes[(j + 1) % 3], false)[0];
+                            CheckBox buf2 = (CheckBox)Controls.Find(players[i].boxes[(j + 2) % 3], false)[0];
+                            buf1.Checked = false; buf2.Checked = false;
+                            bet_state[MatchList.SelectedIndex, i, j] = chk.Checked;
+                            bet_state[MatchList.SelectedIndex, i, (j + 1) % 3] = buf1.Checked;
+                            bet_state[MatchList.SelectedIndex, i, (j + 2) % 3] = buf2.Checked;
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+        public void load_bet_state()
+        {
+            int idx = MatchList.SelectedIndex;
+            bool lock_state = bet_locks[MatchList.SelectedIndex];
+            for (int i = 0; i < Player_nr; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    CheckBox buf = (CheckBox)Controls.Find(players[i].boxes[j], false)[0];
+                    buf.Checked = bet_state[idx, i, j];
+                    buf.Enabled = !lock_state;
+                }
+            }
+        }
+        public FIR()
+        {
+            InitializeComponent();
+        }
+        private void FIR_Load(object sender, EventArgs e)
+        {
+            load_matches_and_players();
+            MatchList.Items.AddRange(Matches);
+            MatchList.SelectedIndex = 0;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string current = MatchList.Items[MatchList.SelectedIndex].ToString();
+            Playing1.Text = current.Split(' ')[1];
+            Playing2.Text = current.Split(' ')[3];
+            load_bet_state();
+        }
+
+        private void Box_click_handler(object sender, EventArgs e)
+        {
+            update_boxes(sender);
+        }
+
+        private void Lock_Click(object sender, EventArgs e)
+        {
+            bet_locks[MatchList.SelectedIndex] = true;
+            for (int i = 0; i < Player_nr; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    CheckBox buf = (CheckBox)Controls.Find(players[i].boxes[j], false)[0];
+                    buf.Enabled = false;
+                }
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (bet_locks[MatchList.SelectedIndex])
+            {
+                int score = (int)score1.Value - (int)score2.Value;
+                if (score < 0)
+                {
+                    scores[MatchList.SelectedIndex] = -1;
+                }
+                if (score == 0)
+                {
+                    scores[MatchList.SelectedIndex] = 2;
+                }
+                if (score > 0)
+                {
+                    scores[MatchList.SelectedIndex] = 1;
+                }
+                for (int i = 0; i < Player_nr; i++)
+                {
+                    if (players[i].Name == Playing1.Text)
+                    {
+                        players[i].Bilans += score;
+                    }
+                    if (scores[MatchList.SelectedIndex] == 1)
+                    {
+                        players[i].wins++;
+                    }
+                    if (scores[MatchList.SelectedIndex] == 2)
+                    {
+                        players[i].draws++;
+                    }
+                    if (scores[MatchList.SelectedIndex] == -1)
+                    {
+                        players[i].loses++;
+                    }
+                }
+                score = -1 * score;
+                for (int i = 0; i < Player_nr; i++)
+                {
+                    if (players[i].Name == Playing2.Text)
+                    {
+                        players[i].Bilans += score;
+                    }
+                    if (scores[MatchList.SelectedIndex] == 1)
+                    {
+                        players[i].wins++;
+                    }
+                    if (scores[MatchList.SelectedIndex] == 2)
+                    {
+                        players[i].draws++;
+                    }
+                    if (scores[MatchList.SelectedIndex] == -1)
+                    {
+                        players[i].loses++;
+                    }
+                }
+                score1.Enabled = false;
+                score2.Enabled = false;
+            }
+        }
+    }
+}
